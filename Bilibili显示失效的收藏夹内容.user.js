1d946ccc50fe0cb14a7f7b2693a1c6ed6bdaa7d9
Liu's DeskTop <wwwlsmcom@msn.cn>
2018-09-21 09:47:52 +0800

    update

--- Bilibili显示失效的收藏夹内容.user.js
+++ Bilibili显示失效的收藏夹内容.user.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         Bilibili显示失效的收藏夹内容
-// @namespace    http://tampermonkey.net/
+// @namespace    https://liu233w.github.io
 // @version      0.1
 // @description  Bilibili显示失效的收藏夹内容
 // @author       Liu233w
+// @license      BSD 3-Clause License
 // @match        https://space.bilibili.com/*
 // @grant        none
+// @downloadURL  https://github.com/Liu233w/userscripts/raw/master/Bilibili%E6%98%BE%E7%A4%BA%E5%A4%B1%E6%95%88%E7%9A%84%E6%94%B6%E8%97%8F%E5%A4%B9%E5%86%85%E5%AE%B9.user.js
+// @updateURL    https://github.com/Liu233w/userscripts/raw/master/Bilibili%E6%98%BE%E7%A4%BA%E5%A4%B1%E6%95%88%E7%9A%84%E6%94%B6%E8%97%8F%E5%A4%B9%E5%86%85%E5%AE%B9.user.js
 // ==/UserScript==
 
 (function() {
