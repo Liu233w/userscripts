959259b6b611024da50acd38b6e7df77a3f8bb26
Liu233w <Liu233w@users.noreply.github.com>
2019-03-08 17:10:42 +0800

    Update Bilibili显示失效的收藏夹内容.user.js

--- Bilibili显示失效的收藏夹内容.user.js
+++ Bilibili显示失效的收藏夹内容.user.js
@@ -2,7 +2,7 @@
 // @name         Bilibili显示失效的收藏夹内容
 // @namespace    https://liu233w.github.io
 // @version      0.1
-// @description  RT，将标题中的“视频已失效”替换成实际的视频名称。
+// @description  RT，将标题中的“视频已失效”替换成实际的视频名称。 ---- 由于B站取消了获取失效视频内容的接口，现在这个脚本已经失效了 ----
 // @author       Liu233w
 // @license      BSD 3-Clause License
 // @match        https://space.bilibili.com/*
