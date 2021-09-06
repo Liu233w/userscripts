2327a2ce877d9e46a1f3971cb6b5114831ff253b
Liu233w <gh@liu233w.com>
2021-09-06 16:21:32 +1000

    Update Go to biliplus.user.js

--- Go to biliplus.user.js
+++ Go to biliplus.user.js
@@ -4,16 +4,31 @@
 // @version      0.1
 // @description  在tucao.one的搜索页面的每个条目添加一个链接，一键跳转到对应的biliplus地址
 // @author       Liu233w
-// @match        https://www.tucao.one/index.php?m=search&*
+// @match        https://www.tucao.one/
 // @icon         https://www.google.com/s2/favicons?domain=tucao.one
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-     $('.list .info').each(function() {
-         const href = $(this).find('a.blue').attr('href')
-         const vid = href.match(/(h\d+)/)[0]
-         $(this).append(`<a href="https://www.biliplus.com/play/${vid}/">BiliPlus</a>`)
-     })
+
+    if (window.location.href.indexOf("www.tucao.one/index.php?m=search&") > -1) {
+        $('.list .info').each(function () {
+            const href = $(this).find('a.blue').attr('href')
+            const vid = href.match(/(h\d+)/)[0]
+            $(this).append(`<a href="https://www.biliplus.com/play/${vid}/">BiliPlus</a>`)
+        })
+
+    } else if (window.location.href.indexOf("www.tucao.one/list/") > -1) {
+        $('.box').each(function () {
+            const href = $(this).find('a.title').attr('href')
+            const vid = href.match(/(h\d+)/)[0]
+            $(this).find('.info').append(`<a href="https://www.biliplus.com/play/${vid}/">BiliPlus</a>`)
+        })
+
+    } else if (window.location.href.indexOf("www.tucao.one/play/") > -1) {
+        const vid = window.location.href.match(/(h\d+)/)[0]
+        $('.new_tool ul').prepend(`<a href="https://www.biliplus.com/play/${vid}/">BiliPlus</a>`)
+    }
+
 })();
